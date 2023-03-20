91663da420244c4c47a2c7ffec8ba4f5f957f3b4
REKA309 <reka3092@gmail.com>
2023-03-20 11:31:34 +0530

    commit change

---
+++ src/components/counter.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useState } from "react";
+
+  export default function Counter()
+{
+    const [count,setCount]=useState(0);
+  function handleChange(type) {
+    if(type === 'decrement' && count >=1)setCount(count -1) ;
+    else if (type === 'increment') setCount(count +1) ;
+  }
+    return(<div className="container">
+    <div className='row'>
+    <div className="counter-container">
+<button className="counter-button" onClick={()=>handleChange('decrement')}>
+-
+</button>
+<div style={{width: 20}}>
+
+</div>
+<p id="counter-value" className="counter-text">
+{count}
+</p>
+<div style={{width: 20}}>
+
+</div>
+<button className="counter-button"  onClick={()=>handleChange('increment')}>
++
+</button>
+</div>
+    </div>
+  </div>);
+}
